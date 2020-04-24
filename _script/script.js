2d77183b56d9ebdacc4dc3f2ed334f85851a3f5a
LuanCesarDC <luancesardc99@gmail.com>
2020-04-24 10:40:32 -0300

    Adicionadas algumas melhorias, Adicionada API Imagem do dia (Nasa)

--- _script/script.js
+++ _script/script.js
@@ -1,12 +1,15 @@
 $(".button_menu").click(function(){
-    $(".menu").show();
-    $(".mensagem").hide();
-    $(".button_menu").hide();
-    $(".logo").hide();
+    $(".menu").toggleClass("show");
 })
-$(".button_close").click(function(){
-    $(".menu").hide();
-    $(".mensagem").show();
-    $(".button_menu").show();
-    $(".logo").show();
-})
+
+$.ajax({
+    url: "https://api.nasa.gov/planetary/apod?api_key=5S0OhioTtOCkLvlYGiKQOEpy9K9mn2aTRRPdzwi8",
+    success: function(whatyougot){
+        if(whatyougot.media_type == "video"){
+            document.getElementById("img_day").innerHTML= "<h2>Vídeo do Dia</h2> <iframe width='315' height='236' src='"+ whatyougot.url +"'> </iframe>"   
+        }else{
+            document.getElementById("img_day").innerHTML="<h2>Imagem do Dia</h2> <img src='"+whatyougot.url+"'style='width:100%;'/>";
+        }
+    }
+});
+
