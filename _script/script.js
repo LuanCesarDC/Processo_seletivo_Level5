ffd4cfb47aaaab38d64f3d6ad850c9e8b754dc18
LuanCesarDC <luancesardc99@gmail.com>
2020-05-03 02:47:04 -0300

    Adicionada a pagina Sobre

--- _script/script.js
+++ _script/script.js
@@ -9,10 +9,11 @@ $.ajax({
     url: "https://api.nasa.gov/planetary/apod?api_key=5S0OhioTtOCkLvlYGiKQOEpy9K9mn2aTRRPdzwi8",
     success: function(whatyougot){
         if(whatyougot.media_type == "video"){
-            document.getElementById("img_day").innerHTML= "<h2>Vídeo do Dia</h2> <iframe width='315' height='236' src='"+ whatyougot.url +"'> </iframe>"   
+            document.getElementById("img_day").innerHTML = "<h2>Vídeo do Dia</h2> <iframe width='315' height='236' src='"+ whatyougot.url +"'> </iframe>"   
         }else{
-            document.getElementById("img_day").innerHTML="<h2>Imagem do Dia</h2> <img src='"+whatyougot.url+"'style='width:100%;'/>";
+            document.getElementById("img_day").innerHTML = "<h2>Imagem do Dia</h2> <img src='"+whatyougot.url+"'style='width:100%;'/>";
         }
+        document.getElementById("img_title").innerText = whatyougot.title;
+        document.getElementById("explanation").innerText = whatyougot.explanation;
     }
 });
-
